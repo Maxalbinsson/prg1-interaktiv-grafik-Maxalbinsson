d5a80799ed9358a8e95d1dbca8c1823cf1143490
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-05-17 14:24:08 +0200

    Gjort mer kod.

--- src/Box.java
+++ src/Box.java
@@ -1,6 +1,8 @@
 import javax.swing.plaf.synth.ColorType;
-import java.awt.*;
-import java.awt.event.KeyEvent;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.*;
 
 public class Box extends Rectangle {
     Color color;
@@ -13,25 +15,28 @@ public class Box extends Rectangle {
         this.color = color;
     }
 
-    public void keypressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            this.y = y - 10;
+    public Box(int x, int y, int width, int height) {
+    }
 
-            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                this.y = y + 10;
-
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    this.x=x-10;
-                    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                        this.x=x+10;
-                    }
-                }
-            }
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+            this.y=y-10;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+            this.y=y+10;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            this.x=x-10;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            this.x=x+10;
         }
     }
 
 
     public void draw(Graphics g){
+        g.setColor(this.color);
+        g.fillRect(this.x,this.y,this.width,this.height);
     }
 
 
--- src/Frame.java
+++ src/Frame.java
@@ -1,3 +1,55 @@
-import javax.swing.JFrame;
-public class Frame extends JFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Frame extends JFrame{
+
+    Image image;
+    Graphics graphics;
+    Box Spelare;
+    Box Fiende;
+    boolean gameOver;
+
+    Frame(){
+        Spelare = new Box(100,300,50,50,Color.blue);
+        Fiende = new Box(400,300,50,50,Color.red);
+        gameOver = false;
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600,600);
+        this.setVisible(true);
+        this.addKeyListener(new AL());
+    }
+
+    public void paint(Graphics g) {
+        image = createImage(this.getWidth(),this.getHeight());
+        graphics = image.getGraphics();
+        g.drawImage(image,0,0,this);
+
+        Spelare.draw(g);
+        Fiende.draw(g);
+
+        if(gameOver) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("MV Boli",Font.PLAIN,45));
+            g.drawString("DU VAN!", 150, 100);
+        }
+    }
+
+    public void checkCollision() {
+        if(Spelare.intersects(Fiende)) {
+            gameOver = true;
+            System.out.println("DU VAN!");
+        }
+    }
+
+    public class AL extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            Spelare.keyPressed(e);
+            checkCollision();
+            repaint();
+        }
+    }
 }
